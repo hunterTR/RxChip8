c1fea4bea87f9145cbf4116824877bea0e4e9fa1
ckaya <ckaya@opers.org>
2019-06-12 12:12:07 -0400

    adding a little rxjs

---
+++ src/renderer.ts
@@ -0,0 +1,31 @@
+const SCREEN_WIDTH = 640;
+const SCREEN_HEIGHT = 320;
+const pixelScale = 10;
+export class Renderer {
+  public screen: HTMLCanvasElement;
+  public canvasContext: CanvasRenderingContext2D;
+  constructor() {
+    this.screen = <HTMLCanvasElement>document.getElementById('screen');
+    this.canvasContext = this.screen.getContext('2d');
+    this.canvasContext.fillStyle = 'black';
+    this.canvasContext.fillRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+  }
+
+  drawScreen(graphicArray: any[][]) {
+    // reset canvas
+    this.canvasContext.fillStyle = 'black';
+    this.canvasContext.fillRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+    // draw
+    for (let h = 0; h < 32; h++) {
+      for (let w = 0; w < 64; w++) {
+        if (graphicArray[h][w] === 1) {
+          this.drawPixel(w, h);
+        }
+      }
+    }
+  }
+  drawPixel(x: number, y: number) {
+    this.canvasContext.fillStyle = 'white';
+    this.canvasContext.fillRect(x * pixelScale, y * pixelScale, 10, 10);
+  }
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/main.ts',
+  entry: './main.ts',
   devtool: 'inline-source-map',
   module: {
     rules: [
